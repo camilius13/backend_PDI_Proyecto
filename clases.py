1ba6dbd864547223d73013d9ed36db60a1ef8da2
camilius13 <pcamilo743@gmail.com>
2026-05-25 14:51:06 -0500

    archivos

---
+++ clases.py
@@ -0,0 +1,13 @@
+CLASES = {
+
+    1:"PDI-Proyecto",
+    2:"accesorio",
+    3:"copa_1",
+    4:"copa_2",
+    5:"estatuilla_1",
+    6:"estatuilla_2",
+    7:"jarron_1",
+    8:"jarron_2"
+    
+
+}
---
+++ museo_data.py
@@ -0,0 +1,112 @@
+MUSEO_DATA = {
+
+    "accesorio":{
+
+        "descripcion":
+        "Objeto utilizado por culturas precolombinas para almacenamiento y actividades ceremoniales.",
+
+        "epoca":
+        "Siglo X",
+
+        "origen":
+        "Región Andina",
+
+        "imagen":
+        "../public/Museo_Lili.jpg"
+
+    },
+
+    "copa_1":{
+
+        "descripcion":
+        "Pieza arqueológica utilizada con fines ceremoniales.",
+
+        "epoca":
+        "Siglo XII",
+
+        "origen":
+        "Valle del Cauca",
+
+        "imagen":
+        "../public/Museo_Lili.jpg"
+
+
+    },
+
+    "copa_2":{
+
+        "descripcion":
+        "Pieza arqueológica utilizada con fines ceremoniales.",
+
+        "epoca":
+        "Siglo VIII",
+
+        "origen":
+        "Zona Pacífica",
+
+        "imagen":
+        "../public/Museo_Lili.jpg"
+
+    },
+    "estatuilla_1":{
+
+        "descripcion":
+        "Figura escultórica que representa deidades o personajes importantes.",
+
+        "epoca":
+        "Siglo IX",
+
+        "origen":
+        "Región Andina",
+
+        "imagen":
+        "../public/Museo_Lili.jpg"
+
+    },
+    "estatuilla_2":{
+
+        "descripcion":
+        "Figura escultórica que representa deidades o personajes importantes.",
+
+        "epoca":
+        "Siglo XI",
+
+        "origen":
+        "Zona Pacífica",
+
+        "imagen":
+        "../public/Museo_Lili.jpg"
+
+    },
+    "jarron_1":{
+
+        "descripcion":
+        "Vasija de cerámica utilizada para almacenamiento y transporte de líquidos.",
+
+        "epoca":
+        "Siglo X",
+
+        "origen":
+        "Región Andina",
+
+        "imagen":
+        "../public/Museo_Lili.jpg"
+
+    },
+    "jarron_2":{
+
+        "descripcion":
+        "Vasija de cerámica utilizada para almacenamiento y transporte de líquidos.",
+
+        "epoca":
+        "Siglo XII",
+
+        "origen":
+        "Zona Pacífica",
+
+        "imagen":
+        "../public/Museo_Lili.jpg"
+
+    }
+
+}
